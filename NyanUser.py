e6cbd89e3a89e08b4239d9d90762c8f6ae94b94b
Dennis Nguyen <nguyendp@ucalgary.ca>
2016-02-27 13:36:19 -0700

    Changed the cooldown system to be a little more friendly to sparse searchers.

--- NyanUser.py
+++ NyanUser.py
@@ -10,8 +10,16 @@ class NyanUser:
     def PerformedSearch(self):
         self.searches += 1
         
+        # If they do a bunch of searches all at once, block them for half an hour.
         if self.searches >= 8:
             threading.Timer(1800.0, self.ResetSearches).start()
-            
+        # Otherwise, the search counter will decay 5 minutes after their search
+        else:
+            threading.Timer(600.0, self.DecrementSearches).start()
+    
+    def DecrementSearches(self):
+        if (self.searches > 0):
+            self.searches -= 1
+    
     def ResetSearches(self):
         self.searches = 0
